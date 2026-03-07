5a3c21b953fa1881b5b0664823f9c554644f390d
Aarti-Rathore1 <aartirathore173@gmail.com>
2026-03-07 19:51:22 +0530

    updated

--- array.py
+++ array.py
@@ -764,7 +764,7 @@ sum = 0
 while temp > 0:
     digit = temp % 10      # gives last number  153 % 10 = 3
     sum = sum + digit**3   # gives cube and add sum
-    temp = temp // 10      
+    temp = temp // 10      # removes last digit  153 // 10 = 15
 
 if sum == num:
     print("Armstrong number")
