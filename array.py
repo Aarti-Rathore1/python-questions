b317c6ecb15039c6c5f1955a3e6d91f3ea0fb341
Aarti-Rathore1 <aartirathore173@gmail.com>
2026-03-09 19:28:42 +0530

    updated

--- array.py
+++ array.py
@@ -810,6 +810,21 @@ print("even numbers: ",even)
 print("odd numbers: ",odd)    '''      
 
 
+# prime or not
+
+n  = int(input("enter element: "))
+
+if n <= 1:
+    print("not prime")
+else:
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            print("not prime")
+            break
+        else:
+            print("prime")    
+
+
 
     
 
