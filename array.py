0b3adbf4f174d2b26d06f190a7558221de892c28
Aarti-Rathore1 <aartirathore173@gmail.com>
2026-03-17 21:00:04 +0530

    edited

--- array.py
+++ array.py
@@ -857,7 +857,7 @@ for i in range(n):
     print(a , end = " ")
     c = a + b
     a = b
-    b = c 
+    b = c '''
 
 # gcd and hcf of two numbers
 
@@ -870,7 +870,7 @@ for i in range (1, min(a , b) + 1):
     if a % i == 0 and b % i == 0:
         gcd = i
 
-print(gcd)    '''    
+print(gcd)        
 
 
 
