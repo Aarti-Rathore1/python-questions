d1592f0fc8a62e316d98fc7925edb6c666e1b503
Aarti-Rathore1 <aartirathore173@gmail.com>
2026-03-18 19:50:14 +0530

    updated

--- tcs nqt prepinsta.py
+++ tcs nqt prepinsta.py
@@ -1,5 +1,5 @@
 # moves all the zeroes to the end
-arr = list(map(int, input("enter  th elements: ").split()))
+'''arr = list(map(int, input("enter  th elements: ").split()))
 
 pos = 0
 
@@ -8,4 +8,9 @@ for i in range(len(arr)):
      arr[pos],arr[i] = arr[i],arr[pos]
      pos += 1
 
-print("AFTER MOVING ALL THE ZEROES TO THE END:",arr)    
+print("AFTER MOVING ALL THE ZEROES TO THE END:",arr)  '''
+
+# decimal to binary
+
+n = int(input("enter elements: "))     # n = 10  , n.bit_length() = 10 -> 1010 -> 4 bits
+print((1 << n.bit_length()) - 1 - n)   # 1 << 4 , 1010 -> 100000 -> 16 -> 16-1 -> 15 - 10 -> 5
