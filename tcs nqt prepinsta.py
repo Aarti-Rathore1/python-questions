1fadc35eefbf7aec9029e12693efc6f33604fe30
Aarti-Rathore1 <aartirathore173@gmail.com>
2026-03-21 18:58:51 +0530

    updated

--- tcs nqt prepinsta.py
+++ tcs nqt prepinsta.py
@@ -14,3 +14,24 @@ print("AFTER MOVING ALL THE ZEROES TO THE END:",arr)
 
 n = int(input("enter elements: "))     # n = 10  , n.bit_length() = 10 -> 1010 -> 4 bits
 print((1 << n.bit_length()) - 1 - n)   # 1 << 4 , 1010 -> 100000 -> 16 -> 16-1 -> 15 - 10 -> 5'''
+
+# counting days problem
+
+def count_sundays(start_day , n):
+    days = {"mon": 0, "tue": 1, "wed": 2, "thur": 3, "fri": 4, "sat": 5, "sun": 6 }
+
+    start_index = days[start_day.lower()]
+
+    #days to reach first sunday
+    days_to_sunday = (6 - start_index) % 7
+
+    # if no sunday within n days
+    if days_to_sunday >= n:
+        return 0
+    
+    #count sundays
+    remaining_days = n - days_to_sunday
+    return 1 +(remaining_days // 7)
+
+#example
+print(count_sundays("tue",20))
